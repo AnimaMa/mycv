324c877ca751187c6f97353663ce4ccfed0135f2
Annamaria M <anm.martincekova@gmail.com>
2021-02-08 02:28:07 +0100

    fixed type declaration

--- client/components/shared/Tooltips/TooltipDelete/TooltipDelete.tsx
+++ client/components/shared/Tooltips/TooltipDelete/TooltipDelete.tsx
@@ -5,7 +5,7 @@ import React from "react"
 
 export interface TooltipDeleteProps {
   className?: string
-  onClick(): () => {}
+  onClick: () => void
 }
 
 const TooltipDeleteInner = (props: TooltipDeleteProps) => {
