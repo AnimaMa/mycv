4fa2ac199fec774e1bd8ea2a9e00ae46cd4342ba
Annamaria M <anm.martincekova@gmail.com>
2021-02-12 14:51:22 +0100

    Removed min width

--- client/components/Resume/Aside/BasicsSection/components/Address.tsx
+++ client/components/Resume/Aside/BasicsSection/components/Address.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import { useResume } from "../../../ResumeProvider/ResumeProvider";
-import { IoMdPin } from "react-icons/io";
+import React from "react"
+import { useResume } from "../../../ResumeProvider/ResumeProvider"
+import { IoMdPin } from "react-icons/io"
 
 export const Address = () => {
-  const { basics } = useResume();
+  const { basics } = useResume()
 
   return (
     <div className="my-4">
       {basics.location && (
         <div className="my-4">
           <div className="flex  items-center space-x-2">
-            <IoMdPin className="text-base mr-1 min-w-min text-secondary" />
+            <IoMdPin className="text-base mr-1 text-secondary" />
             <div>
               <p className="mb-2 font-thin">{basics.location.address}</p>
               <p className="mb-2 font-thin">{`${basics.location.city}, ${basics.location.postalCode}`}</p>
@@ -20,5 +20,5 @@ export const Address = () => {
         </div>
       )}
     </div>
-  );
-};
+  )
+}
--- client/components/Resume/Aside/BasicsSection/components/ContactInfo.tsx
+++ client/components/Resume/Aside/BasicsSection/components/ContactInfo.tsx
@@ -1,22 +1,22 @@
-import React from "react";
-import { useResume } from "../../../ResumeProvider/ResumeProvider";
-import { HiOutlineMail, HiOutlinePhone } from "react-icons/hi";
+import React from "react"
+import { useResume } from "../../../ResumeProvider/ResumeProvider"
+import { HiOutlineMail, HiOutlinePhone } from "react-icons/hi"
 
 export const ContactInfo = () => {
-  const { basics } = useResume();
+  const { basics } = useResume()
 
   return (
     <div className="mt-8">
       {basics.email && (
         <div className="my-4 flex justify-center flex-col">
           <div className=" flex flex-row space-x-2">
-            <HiOutlineMail className="text-base mr-1 min-w-min text-secondary" />
+            <HiOutlineMail className="text-base mr-1  text-secondary" />
             <a href={`mailto:${basics.email}`} className=" text-primary hover:text-secondary text-base ">
               {basics.email}
             </a>
           </div>
           <div className="  flex flex-row space-x-2">
-            <HiOutlinePhone className="text-base mr-1  min-w-min text-secondary" />
+            <HiOutlinePhone className="text-base mr-1   text-secondary" />
             <a href={`tel:${basics.phone}`} className=" text-primary hover:text-secondary text-base">
               {basics.phone}
             </a>
@@ -24,5 +24,5 @@ export const ContactInfo = () => {
         </div>
       )}
     </div>
-  );
-};
+  )
+}
